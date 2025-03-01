bd4430e54f17ddb5978553ed251f4b03317098ff
codewins95 <mks957678@gmail.com>
2025-03-01 14:03:48 +0530

    error solve

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/owncomponnets/Header/Header";
 import Footer from "@/owncomponnets/footer/Footer";
 // import { SmoothScroll } from "@/owncomponnets/Locomotive";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Sampoorna",
@@ -27,18 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-slate-900 antialiased`}>
-
-
+      <body className={` bg-slate-900 antialiased`}>
         <Header />
-
-
         <main className="pt-30">
           {children}
         </main>
-
-
         <Footer />
       </body>
     </html>
